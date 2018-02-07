128b3322390668b8adb5c752cffce9412a5fc2c6
Bilger Yahov <bayahov1@gmail.com>
2018-02-07 13:40:42 +0100

    Fetch character with a prop. Implement switching characters.

--- applications/starbase/src/components/Character.vue
+++ applications/starbase/src/components/Character.vue
@@ -1,27 +1,32 @@
 <template>
-	<div class="">
+	<div class="" @click="switchCharacter">
 		{{ character }}
 	</div>
 </template>
 
 <script>
 	export default {
+		props: ['id'],
 		data() {
 			return {
 				character: {}
 			}
 		},
 		methods: {
-			fetchCharacter() {
-				fetch('https://swapi.co/api/people/1/', {
+			fetchCharacter(id) {
+				fetch(`https://swapi.co/api/people/${id}/`, {
 					method: 'GET'
 				})
 				.then(res => res.json())
 				.then(data => this.character = data);
+			},
+			switchCharacter() {
+				let randomID = Math.floor(Math.random() * 83)+ 1;
+				this.fetchCharacter(randomID);
 			}
 		},
 		created() {
-			this.fetchCharacter();
+			this.fetchCharacter(this.id);
 		}
 	}
 </script>
