31526eab819ff50fdd387ba44b40876dc9eda51a
Bilger Yahov <bayahov1@gmail.com>
2018-02-08 11:01:11 +0100

    Fix references to files. Use first actions and mutations in App component.

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -1,7 +1,19 @@
 <template>
 	<div id="app">
-		Got jokes?
-		<br />
-		{{ $store.state }}
+		<h3>Got jokes?</h3>
+		<button class="btn btn-primary" @click="initJokes">Get Ten Random Jokes</button>
+		<div>
+			{{ $store.state }}
+		</div>
 	</div>
 </template>
+
+<script>
+	import { mapActions } from 'vuex';
+
+	export default {
+		methods: mapActions([
+			'initJokes'
+		])
+	}
+</script>
--- applications/jokester/src/store/actions.js
+++ applications/jokester/src/store/actions.js
@@ -1,4 +1,4 @@
-import * as types from 'mutation-types';
+import * as types from './mutation-types';
 
 export const initJokes = ({ commit }) => {
 	fetch('https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_ten', {
