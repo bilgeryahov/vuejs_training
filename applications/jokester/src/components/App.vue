aa1039380fd9b19094c69312c3d9eb137d5bb041
Bilger Yahov <bayahov1@gmail.com>
2018-02-08 11:13:39 +0100

    Add a Joke component. Use the new component in the App.

--- applications/jokester/src/components/App.vue
+++ applications/jokester/src/components/App.vue
@@ -2,18 +2,29 @@
 	<div id="app">
 		<h3>Got jokes?</h3>
 		<button class="btn btn-primary" @click="initJokes">Get Ten Random Jokes</button>
-		<div>
-			{{ $store.state }}
+		<br />
+		<br />
+		<div class="row">
+			<Joke
+				v-for="(joke, index) in $store.state.jokes"
+				:joke="joke"
+				:key="index"
+			/>
 		</div>
 	</div>
 </template>
 
 <script>
 	import { mapActions } from 'vuex';
+	import Joke from './Joke';
+
 
 	export default {
 		methods: mapActions([
 			'initJokes'
-		])
+		]),
+		components: {
+			Joke
+		}
 	}
 </script>
