36626e68b09820896732fbbf5433553486e39414
Bilger Yahov <bayahov1@gmail.com>
2018-02-08 14:47:30 +0100

    Create a firebase reference to the real-time database.

--- applications/turnout/src/firebaseApp.js
+++ applications/turnout/src/firebaseApp.js
@@ -10,3 +10,8 @@ const config = {
 };
 
 export const firebaseApp = firebase.initializeApp(config);
+
+export const eventsRef = firebaseApp
+									.database()
+									.ref()
+									.child('events');
