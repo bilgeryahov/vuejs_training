406e551483dfc5500644920fc137c8afc08c85f8
Bilger Yahov <bayahov1@gmail.com>
2018-02-08 13:19:17 +0100

    Add basic HTML markup for the SignIn component. Make sure to add 'email' and 'password' data fields.

--- applications/turnout/src/components/SignIn.vue
+++ applications/turnout/src/components/SignIn.vue
@@ -1,5 +1,31 @@
 <template>
-	<div class="">
-		SignIn Component.
+	<div class="from-inline">
+		<h3>Sign In</h3>
+		<div class="form-group">
+			<input
+				type="text"
+				placeholder="email"
+				class="form-control"
+				v-model="email"
+			/>
+			<input
+				type="password"
+				placeholder="password"
+				class="form-control"
+				v-model="password"
+			/>
+			<button class="btn btn-primary">Sign In</button>
+		</div>
 	</div>
 </template>
+
+<script>
+	export default {
+		data() {
+			return {
+				email: '',
+				password: ''
+			}
+		}
+	}
+</script>
