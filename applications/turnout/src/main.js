be98ab31f5e1ebd06dd346f7a78e97d62ea56079
Bilger Yahov <bayahov1@gmail.com>
2018-02-08 13:02:21 +0100

    Set up routing. Set up new components.

--- applications/turnout/src/main.js
+++ applications/turnout/src/main.js
@@ -1,7 +1,22 @@
 import Vue from 'vue';
+import VueRouter from 'vue-router';
+
 import App from './components/App';
+import Dashboard from './components/Dashboard';
+import SignIn from './components/SignIn';
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+	mode: 'history',
+	routes: [
+		{ path: '/dashboard', component: Dashboard },
+		{ path: '/signin', component: SignIn }
+	]
+});
 
 new Vue({
 	el: '#app',
+	router,
 	render: h => h(App)
 });
